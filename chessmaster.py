df321ea435269a7ffe438c91c8e3b14f1b3a7c5f
Erica Liz <erica.liz@spsmail.cuny.edu>
2015-04-19 02:41:03 +0000

    made changes to chessmaster

--- chessmaster.py
+++ chessmaster.py
@@ -6,16 +6,17 @@ import time
 
 
 class ChessPiece(object):
-    """Docstring.
-
-    """
+    """Moving piece in a game of chess."""
     prefix = ''
 
     def __init__(self, position):
         """Docstring.
-
+        Args:
+            position: an alphanumeric position on the board.
+        Attribute:
+            prefix: an empty string by default.
         """
-        if not ChessPiece.is_legal_move(self, position): #testing legal move
+        if not ChessPiece.is_legal_move(self, position):
             excep = '{} is not a legal start position'
             raise ValueError(excep.format(position))
         else:
@@ -23,43 +24,43 @@ class ChessPiece(object):
             self.moves = []
 
     def algebraic_to_numeric(self, tile):
-        """Docstring.
-
+        """A tile as a two value tuple used as position.
+        Arg:
+            tile: tuple consisting two value of the position on the board.
         """
-        x_axis = ('abcdefgh')
+        x_axis = 'abcdefgh'
         y_axis = [1, 2, 3, 4, 5, 6, 7, 8]
-        if len(tile) > 2:
-            return None
+        if tile[0] in x_axis and int(tile[1]) in y_axis:
+            return (x_axis.find(tile[0]), (int(tile[1]) - 1))
         else:
-            if x_axis[0] and int(y_axis[1]) in tile:
-                return (x_axis.find(x_axis[0]), (int(y_axis[1]) - 1))
-            else:
-                return None
+            return None
 
     def is_legal_move(self, position):
-        """Docstring.
-
+        """To test if new position is legal to move.
+        Args:
+            position: an allowed position on the board.
+        Attribute:
+            newpos: a new position on the board.
         """
         newpos = self.algebraic_to_numeric
         if newpos(position):
             return True
         else:
             return False
-        #if test not in ChessPiece.position():
-            #excep = '{} is not a legal start position'
-            #raise ValueError(excep.format(position))
-            
 
-    #def move(self, position):
-        """Docstring.
-
+    def move(self, position):
+        """Pieces to move.
+        Args:
+            position: an allowed position on the board.
+        Attribute:
+            newpos: a new position on the board.
         """
-        #newpos = self.is_legal_move(position)
-        #if newpos:
-            #tup = (self.prefix + position, self.prefix + position, time.time()
-            #return tup
-        #else:
-            #return False
-
-
-        
+        newpos = self.is_legal_move(position)
+        if newpos:
+            tup = (self.prefix + self.position,
+                   self.prefix + position, time.time())
+            self.moves.append(tup)
+            self.position = position
+            return tup
+        else:
+            return False
